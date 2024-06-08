22cbb6ff8032d3d68dbeb0795ec47a67d2aed0a5
MANISH  PATIDAR <102138422+Manish-Patidar50@users.noreply.github.com>
2024-06-08 21:04:44 +0530

    Add files via upload

---
+++ 21.SquareRoot.js
@@ -0,0 +1,24 @@
+
+
+function findSquareRoot()
+{
+   let squareRoot = Math.sqrt();
+   return squareRoot;
+}
+
+
+let number = 25;
+let y = -130;
+let z = 'javaScript';
+{
+    let output = findSquareRoot(number);
+
+console.log("The output of giving number is : " + Math.sqrt(number));
+
+// const y = findSquareRoot(y); 
+console.log("The output of giving value is : " + Math.sqrt(y));
+
+ // const z = findSquareRoot(z);
+console.log("The output of giving value is : " + Math.sqrt (z));
+
+}
