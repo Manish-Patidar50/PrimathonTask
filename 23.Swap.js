51988cb10c3bdd6d71f0b14a337276be0a5ec1f8
MANISH  PATIDAR <102138422+Manish-Patidar50@users.noreply.github.com>
2024-06-08 21:05:38 +0530

    Add files via upload

---
+++ 23.Swap.js
@@ -0,0 +1,17 @@
+
+
+
+    let i = 20;
+    let j = 30;
+    let temp;
+
+    console.log("Before swapping :"+i);
+    console.log("Before swapping : "+j);
+
+    temp = i;
+    i = j;
+    j = temp;
+
+    console.log("After swapping :"+i);
+    console.log("After swapping :" +j);
+
