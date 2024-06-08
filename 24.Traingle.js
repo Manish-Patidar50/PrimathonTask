164d1af68fc349b628647fc414e2bc66e5bd2398
MANISH  PATIDAR <102138422+Manish-Patidar50@users.noreply.github.com>
2024-06-08 21:06:06 +0530

    Add files via upload

---
+++ 24.Traingle.js
@@ -0,0 +1,11 @@
+
+function traingleArea(base,height){
+
+    return 0.5 * base * height;
+}
+
+let base = 20;
+let height = 15;
+let area = traingleArea(base,height);
+
+console.log("The Area of the traingle",area);
