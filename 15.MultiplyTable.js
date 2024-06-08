a3ddf4c3e627f2cb8fd63ca5b469e37d34d66a24
MANISH  PATIDAR <102138422+Manish-Patidar50@users.noreply.github.com>
2024-06-08 21:00:17 +0530

    Add files via upload

---
+++ 15.MultiplyTable.js
@@ -0,0 +1,15 @@
+
+function MultiplyTable(number){
+
+    for(let i = 1; i<=10; i++){
+
+        let number = 11;
+        let result = number * i;
+
+        console.log("The multiplication of table is :", result);
+    }
+}
+
+let number = 11;
+
+MultiplyTable(number);
