ba71860562187a0b561dae828f654dc40604e63e
MANISH  PATIDAR <102138422+Manish-Patidar50@users.noreply.github.com>
2024-06-09 18:22:06 +0530

    Add files via upload

---
+++ 31.DecimalToBinary.js
@@ -0,0 +1,11 @@
+
+function  decimalToBinary(val){
+
+    return val.toString(2);
+
+}
+
+let n = 200;
+let result = decimalToBinary(n);
+
+console.log("After conversion Decimal To Binary :-",result);
