ac32613727fd1dd13b6a58440df29fcf1bad1d88
MANISH  PATIDAR <102138422+Manish-Patidar50@users.noreply.github.com>
2024-06-10 15:29:23 +0530

    Add files via upload

---
+++ 34.SortAlpha.js
@@ -0,0 +1,12 @@
+
+const word = ["Primathon",
+    "Academy",
+    "React",
+    "Css",
+    "Html",
+    "DSA",
+    "IT Company"];
+
+    const AfterSort = word.sort();
+
+    console.log(AfterSort);
