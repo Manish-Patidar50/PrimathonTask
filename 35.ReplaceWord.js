e9524f0fdb90d05c350933afe5f1f7b234a58a2e
MANISH  PATIDAR <102138422+Manish-Patidar50@users.noreply.github.com>
2024-06-10 15:29:46 +0530

    Add files via upload

---
+++ 35.ReplaceWord.js
@@ -0,0 +1,10 @@
+
+let str = 'Welcome to Moreyeahs Technology';
+
+let replStr = 'Moreyeahs Technology';
+
+let newStr = 'Primathon Academy Tech. Pvt. ltd. Indore';
+
+let UpdateStr = str.replace(replStr,newStr);
+
+console.log(UpdateStr);
